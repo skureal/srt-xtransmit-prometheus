4279fb53771041917bc5a1d2838f79dac3c2a29d
skureal <elgono@skureal.de>
2026-09-15 15:28:29 +0200

    Rename binary to srt-xtransmit-prometheus

--- xtransmit/xtransmit-app.cpp
+++ xtransmit/xtransmit-app.cpp
@@ -120,10 +120,20 @@ int main(int argc, char** argv)
 {
 	using namespace xtransmit;
 
-	CLI::App app("SRT xtransmit tool. SRT library v" SRT_VERSION_STRING);
+	CLI::App app(
+		"SRT xtransmit tool with integrated Prometheus exporter. "
+		"SRT library v" SRT_VERSION_STRING);
 	app.set_config("--config");
 	app.set_help_all_flag("--help-all", "Expand all help");
-	app.set_version_flag("--version", string("srt-xtransmit v0.3.0.\nSRT library v") + SRT_VERSION_STRING + " clock " + srt_clock_type_str());
+	app.set_version_flag(
+    "--version",
+    string(
+        "srt-xtransmit-prometheus v0.1.0\n"
+        "Based on srt-xtransmit v0.3.0\n"
+        "SRT library v")
+        + SRT_VERSION_STRING
+        + " clock "
+        + srt_clock_type_str());
 
 	spdlog::set_pattern("%H:%M:%S.%f %^[%L]%$ %v");
 	app.add_flag_function(
